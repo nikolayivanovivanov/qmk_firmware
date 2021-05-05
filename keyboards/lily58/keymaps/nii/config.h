1febabf1ca396055a0c0193f508ef8b8a42261fc
Nikolay Ivanov <nikolay@appsofart.com>
2021-05-05 21:09:35 +0300

    working

--- keyboards/lily58/keymaps/nii/config.h
+++ keyboards/lily58/keymaps/nii/config.h
@@ -29,6 +29,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define USE_SERIAL_PD2
 
+#define ONESHOT_TIMEOUT 3000
+
 #define TAPPING_FORCE_HOLD
 #define TAPPING_FORCE_HOLD_PER_KEY
 #define TAPPING_TERM 200
